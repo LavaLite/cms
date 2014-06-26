eec4b8b9bf97fb29c51e7193b187fc264180356f
George John <info@renfos.com>
2014-06-26 14:49:38 +0530

    Release

--- app/views/public/home.blade.php
+++ app/views/public/home.blade.php
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-<h1>{{$page->heading}}</h1>
-<p>{{$page->content}}</p>
-=======
+
 <div class="jumbotron">
     <h1>Lavalite </h1>
-    <h3>CMS based on Laravel 4.1 & Bootstrap 3.1 </h3>
+    <h3>CMS based on Laravel 4 &amp; Bootstrap 3 </h3>
     <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
     <p><a class="btn btn-lg btn-success" href="register" role="button">Sign up today</a></p>
 </div>
@@ -15,4 +12,3 @@
       <p>{{$page->content}}</p>
   </div>
 </div>
->>>>>>> dev
