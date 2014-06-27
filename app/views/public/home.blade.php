5af55dcd51089845f0747625477b1ea9fcef4eab
George John <info@renfos.com>
2014-06-27 15:06:22 +0530

    Cms update

--- app/views/public/home.blade.php
+++ app/views/public/home.blade.php
@@ -5,7 +5,6 @@
     <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
     <p><a class="btn btn-lg btn-success" href="register" role="button">Sign up today</a></p>
 </div>
-    {{Slider::slider('bootstrap')}}
 <div class="row">
     <div class="col-lg-12">
       <h1>{{$page->heading}}</h1>
