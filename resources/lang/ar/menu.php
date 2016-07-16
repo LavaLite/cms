98b4c4fe2c91857f95233b0a74dda7101e3e208b
Mohammed Zayan <zayanit@gmail.com>
2016-07-16 20:08:06 +0200

    remove the headers

--- resources/lang/ar/menu.php
+++ resources/lang/ar/menu.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * Copyright (c) 2016 MOHAMMED ZAYAN Design, All Rights Reserved.
- *
- * @author        Mohammed Zayan <zayanit@gmail.com>
- * @link          http://www.zayanit.com
- * @version       1.0
- * Created by Sublime Text 3
- */
-
-
 return [
     /*
     |--------------------------------------------------------------------------
