32d2c505945e59b57d2abf25315b672fa8a06af9
Mohammed Zayan <zayanit@gmail.com>
2016-07-16 15:56:27 +0200

    add arabic language translation files

---
+++ resources/lang/ar/messages.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Copyright (c) 2016 MOHAMMED ZAYAN Design, All Rights Reserved.
+ *
+ * @author        Mohammed Zayan <zayanit@gmail.com>
+ * @link          http://www.zayanit.com
+ * @version       1.0
+ * Created by Sublime Text 3
+ */
+
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language files for Messages
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default for all packages
+    |
+     */
+
+    'success'       => [
+        'created' => ':Module تم إضافته بنجاح.',
+        'updated' => ':Module تم تعديله بنجاح.',
+        'deleted' => ':Module تم حذفه بنجاح.',
+        'create'  => ':Module تم إضافته بنجاح.',
+        'update'  => ':Module تم تعديله بنجاح.',
+        'delete'  => ':Module تم حذفه بنجاح.',
+    ],
+
+    'error'         => [
+        'create' => ':Module ﻻ يمكن إضافته.',
+        'update' => ':Module ﻻ يمكن حذفه.',
+        'delete' => ':Module ﻻ يمكن تعديله.',
+    ],
+
+    'unprocessable' => 'كيان ﻻ يمكن معالجته.',
+    'areyousure'    => 'هل أنت متأكد?',
+    'type'          => [
+        'success' => 'نجاح',
+        'error'   => 'خطأ',
+    ],
+];
---
+++ resources/lang/ar/passwords.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Copyright (c) 2016 MOHAMMED ZAYAN Design, All Rights Reserved.
+ *
+ * @author        Mohammed Zayan <zayanit@gmail.com>
+ * @link          http://www.zayanit.com
+ * @version       1.0
+ * Created by Sublime Text 3
+ */
+
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'كلمة المرور يجب أن تكون على اﻷقل 6 أحرف وتوافق تأكيد كلمة المرور.',
+    'reset'    => 'تم إعادة تعيين كلمة المرور الخاصة بك!',
+    'sent'     => 'تم إرسال بريد برابط ﻹعادة تعيين كلمة المرور!',
+    'token'    => 'رمز إعادة تعيين كلمة المرور غير صالح.',
+    'user'     => "ﻻ يوجد مستخدم بهذا البريد اﻹلكترونى.",
+
+];
