dda80325dd6108a9f68c0d9e16d23525615d7bc7
You85 <30325749+You85@users.noreply.github.com>
2017-08-19 20:34:34 +0200

    Update messages.php

--- resources/lang/fr/messages.php
+++ resources/lang/fr/messages.php
@@ -1,1 +1,29 @@
 
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Language files for Messages
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default for all packages
+    |
+     */
+    'success'       => [
+        'created' => ':Module has been added successfully.',
+        'updated' => ':Module has been updated successfully.',
+        'deleted' => ':Module has been deleted successfully.',
+        'changed' => ':Module has been  :status.',
+    ],
+    'error'         => [
+        'create' => ':Module cannot be created.',
+        'update' => ':Module cannot be deleted.',
+        'delete' => ':Module cannot be updated.',
+    ],
+    'unprocessable' => 'Unprocessable entry.',
+    'areyousure'    => 'Are you sure?',
+    'type'          => [
+        'success' => 'Success',
+        'error'   => 'Error',
+    ],
+];
