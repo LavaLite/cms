f135bd291f58ead97f9a8a2f470c3ee1d546d859
You85 <30325749+You85@users.noreply.github.com>
2017-08-22 09:36:39 +0200

    Update passwords.php

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -11,9 +11,9 @@ return [
     | has failed, such as for an invalid token or invalid new password.
     |
     */
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset'    => 'Your password has been reset!',
-    'sent'     => 'We have e-mailed your password reset link!',
-    'token'    => 'This password reset token is invalid.',
-    'user'     => "We can't find a user with that e-mail address.",
+    'password' => 'Les mots de passe doivent comporter au moins six caractères et correspondre à la confirmation.',
+    'reset'    => 'Votre mot de passe a été réinitialisé!',
+    'sent'     => 'Nous avons envoyé votre lien de réinitialisation par mot de passe!',
+    'token'    => 'Ce jeton de réinitialisation de mot de passe n'est pas valide.',
+    'user'     => "Nous ne pouvons pas trouver un utilisateur avec cette adresse e-mail.",
 ];
