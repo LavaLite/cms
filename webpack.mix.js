3bebfeef83dbcd70d119382881984e081d4a7e9b
George John <george@renfos.com>
2017-11-29 22:27:29 +0530

    Updated webpack.

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,14 +14,19 @@ let mix = require('laravel-mix');
 mix.copy('./node_modules/jquery/dist/jquery.min.js', './public/assets/vendor/jquery/jquery.min.js');
 
 // compile admin assets
-mix.setPublicPath('./public/themes/admin/assets/dist');
-mix.js('./public/themes/admin/assets/js/app.js', 'js')
-   .sass('./public/themes/admin/assets/sass/app.scss', 'css/styles.css');
+// mix.setPublicPath('./public/themes/admin/assets/dist');
+// mix.js('./public/themes/admin/assets/js/app.js', 'js')
+//    .sass('./public/themes/admin/assets/sass/app.scss', 'css/styles.css');
 
-// compile user assets
-// mix.setPublicPath('./public/themes/user/assets/dist');
-// mix.js('./public/themes/user/assets/js/app.js', 'js')
-//    .sass('./public/themes/user/assets/sass/app.scss', 'css/styles.css');
+//compile user assets
+mix.setPublicPath('./public/themes/user/assets/dist');
+mix.js('./public/themes/user/assets/js/app.js', 'js')
+   .sass('./public/themes/user/assets/sass/app.scss', 'css/styles.css');
+
+//compile user assets
+// mix.setPublicPath('./public/themes/client/assets/dist');
+// mix.js('./public/themes/client/assets/js/app.js', 'js')
+//    .sass('./public/themes/client/assets/sass/app.scss', 'css/styles.css');
 
 // compile public assets
 // mix.setPublicPath('./public/themes/public/assets/dist');
