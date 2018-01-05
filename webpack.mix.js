aff9fe83fc24d344ddc402d595574b5a52199dfd
George John <george@renfos.com>
2018-01-05 21:42:11 +0530

    Updated javascript packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,9 +19,9 @@ mix.copy('./node_modules/jquery/dist/jquery.min.js', './public/assets/vendor/jqu
 //    .sass('./public/themes/admin/assets/sass/app.scss', 'css/styles.css');
 
 //compile user assets
-mix.setPublicPath('./public/themes/user/assets/dist');
-mix.js('./public/themes/user/assets/js/app.js', 'js')
-   .sass('./public/themes/user/assets/sass/app.scss', 'css/styles.css');
+// mix.setPublicPath('./public/themes/user/assets/dist');
+// mix.js('./public/themes/user/assets/js/app.js', 'js')
+//    .sass('./public/themes/user/assets/sass/app.scss', 'css/styles.css');
 
 //compile client assets
 // mix.setPublicPath('./public/themes/client/assets/dist');
@@ -29,7 +29,7 @@ mix.js('./public/themes/user/assets/js/app.js', 'js')
 //    .sass('./public/themes/client/assets/sass/app.scss', 'css/styles.css');
 
 // compile public assets
-// mix.setPublicPath('./public/themes/public/assets/dist');
-// mix.js('./public/themes/public/assets/js/app.js', 'js')
-//    .sass('./public/themes/public/assets/sass/app.scss', 'css/styles.css');
+mix.setPublicPath('./public/themes/public/assets/dist');
+mix.js('./public/themes/public/assets/js/app.js', 'js')
+   .sass('./public/themes/public/assets/sass/app.scss', 'css/styles.css');
 
