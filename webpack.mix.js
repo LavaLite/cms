a824f92f13cb26393c6dde0d8805047d59e91f5b
George John <george@renfos.com>
2018-01-29 18:12:40 +0530

    updated laravel mix packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,10 @@
 let mix = require('laravel-mix');
 
+mix.webpackConfig({
+    externals: {
+        "jquery": "jQuery"
+    }
+}).options({ processCssUrls: false });
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -8,28 +13,36 @@ let mix = require('laravel-mix');
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
- |
+ | If you want to compile assets for a particulat theme
+ | uncomment the section and run the required command.
  */
 
 mix.copy('./node_modules/jquery/dist/jquery.min.js', './public/assets/vendor/jquery/jquery.min.js');
 
-// compile admin assets
-// mix.setPublicPath('./public/themes/admin/assets/dist');
-// mix.js('./public/themes/admin/assets/js/app.js', 'js')
-//    .sass('./public/themes/admin/assets/sass/app.scss', 'css/styles.css');
+// Admin theme  assets
+mix.setPublicPath('./public/themes/admin/assets/dist');
+mix.js('./public/themes/admin/assets/js/app.js', 'js')
+   .copyDirectory('./node_modules/font-awesome/fonts', './public/themes/admin/assets/dist/fonts')
+   .copyDirectory('./node_modules/ionicons/dist/fonts', './public/themes/admin/assets/dist/fonts')
+   .sass('./public/themes/admin/assets/sass/app.scss', 'css/styles.css');
 
-//compile user assets
+// User theme assets
 // mix.setPublicPath('./public/themes/user/assets/dist');
 // mix.js('./public/themes/user/assets/js/app.js', 'js')
-//    .sass('./public/themes/user/assets/sass/app.scss', 'css/styles.css');
+// 	.copyDirectory('./node_modules/font-awesome/fonts', './public/themes/user/assets/dist/fonts')
+// 	.copyDirectory('./node_modules/ionicons/dist/fonts', './public/themes/user/assets/dist/fonts')
+// 	.sass('./public/themes/user/assets/sass/app.scss', 'css/styles.css');
 
-//compile client assets
+// Client theme assets
 // mix.setPublicPath('./public/themes/client/assets/dist');
 // mix.js('./public/themes/client/assets/js/app.js', 'js')
-//    .sass('./public/themes/client/assets/sass/app.scss', 'css/styles.css');
+// 	.copyDirectory('./node_modules/font-awesome/fonts', './public/themes/client/assets/dist/fonts')
+// 	.copyDirectory('./node_modules/ionicons/dist/fonts', './public/themes/client/assets/dist/fonts')
+// 	.sass('./public/themes/client/assets/sass/app.scss', 'css/styles.css');
 
-// compile public assets
-mix.setPublicPath('./public/themes/public/assets/dist');
-mix.js('./public/themes/public/assets/js/app.js', 'js')
-   .sass('./public/themes/public/assets/sass/app.scss', 'css/styles.css');
+// Public theme assets
+// mix.setPublicPath('./public/themes/public/assets/dist');
+// mix.js('./public/themes/public/assets/js/app.js', 'js')
+// 	.copyDirectory('./node_modules/font-awesome/fonts', './public/themes/public/assets/dist/fonts')
+// 	.sass('./public/themes/public/assets/sass/app.scss', 'css/styles.css');
 
