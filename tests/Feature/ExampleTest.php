f24d08ba6de20173556bd608d64cd4c5c1bfc5ca
George John <george@renfos.com>
2018-10-20 20:33:51 +0530

    Updated test files.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,8 +13,6 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $response = $this->get('/')->assertOk();
     }
 }
