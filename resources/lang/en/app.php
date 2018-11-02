eae757a2bebd2c65c6437916d48944d07477bb36
George John <george@renfos.com>
2018-11-02 04:41:30 +0000

    Apply fixes from StyleCI

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -14,7 +14,7 @@ return [
     'name.short'     => '<b>L</b>l',
     'admin.panel'    => 'Admin Panel',
     'dashboard'      => 'Dashboard',
-    'rights'     => '<a href="https://lavalite.org/" target="_blank">Lavalite</a> &copy; 2018 All Rights Reserved, Built with <a href="https://laravel.com" target="_blank"><i class="fab fa-laravel"></i></a>, Created by <a href="http://renfos.com/" target="_blank">Renfos</a>.',
+    'rights'         => '<a href="https://lavalite.org/" target="_blank">Lavalite</a> &copy; 2018 All Rights Reserved, Built with <a href="https://laravel.com" target="_blank"><i class="fab fa-laravel"></i></a>, Created by <a href="http://renfos.com/" target="_blank">Renfos</a>.',
     'version'        => '<b>Version</b> Develop',
 
     'add'            => 'Add',
