4fffd400b1a690868636db46f2f794979afb4f92
younesi <mehdi.younesi7@gmail.com>
2019-07-15 00:07:24 +0430

    Add Fa(Persian) language fiels

---
+++ resources/lang/fa/app.php
@@ -0,0 +1,80 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Label language files for All Module
+    |--------------------------------------------------------------------------
+    |
+     */
+
+    'name'           => 'لاوالایت',
+    'name.html'      => '<b>لاوالایت</b>',
+    'name.short'     => '<b>ل</b>',
+    'admin.panel'    => 'پنل مدیریت',
+    'dashboard'      => 'داشبورد',
+    'dashboard'      => 'داشبورد',
+    'rights'     => '<a href="https://lavalite.org/" target="_blank">لاوالایت</a> &copy; 2019 همه حقوق محفوظ هست,
+    ساخته شده با <a href="https://laravel.com" target="_blank"><i class="fab fa-laravel"></i>لاراول</a>,
+     بوسیله <a href="http://renfos.com/" target="_blank">Renfos</a>.',
+    'version'        => '<b>ورژن</b> توسعه',
+
+    'add'            => 'اضافه کردن',
+    'addnew'         => 'اضافه کردن آیتم جدید',
+    'actions'        => 'عملیات',
+    'approve'        => 'تایید',
+    'archive'        => 'آرشیو شده',
+    'unarchive'      => 'آرشیو نشده',
+    'back'           => 'عقب',
+    'cancel'         => 'لغو',
+    'close'          => 'بستن',
+    'copy'           => 'کپی',
+    'complete'       => 'کامل شده',
+    'create'         => 'ایجاد',
+    'dashboard'      => 'داشبورد',
+    'delete'         => 'حذف',
+    'draft'          => 'پیش نویس',
+    'details'        => 'جزییات',
+    'edit'           => 'ویرایش',
+    'enterkeyword'   => 'لطفا یک کلمه برای جستجو وارد کنید',
+    'go'             => 'برو',
+    'help'           => 'کمک',
+    'home'           => 'خانه',
+    'list'           => 'لیست',
+    'logout'         => 'خروج',
+    'logs'           => 'لاگها',
+    'manage'         => 'مدیریت',
+    'more'           => 'بیشتر',
+    'new'            => 'جدید',
+    'no'             => 'خیر',
+    'opt'            => 'گزینه ها',
+    'option'         => 'گزینه',
+    'options'        => 'گزینه ها',
+    'order'          => 'سفارش',
+    'password'       => 'رمز عبور',
+    'profile'        => 'پروفایل',
+    'publish'        => 'منتشر شده',
+    'request'        => 'درخواست',
+    'reports'        => 'گزارش ها',
+    'reset'          => 'بازگردانی',
+    'save'           => 'ذخیره',
+    'search'         => 'جستجو',
+    'settings'       => 'تنظیمات',
+    'show'           => 'نمایش',
+    'status'         => 'وضعیت',
+    'update'         => 'ویرایش',
+    'update_profile' => 'بروزرسانی پروفایل',
+    'unpublish'      => 'منتشر نشده',
+    'view'           => 'مشاهده',
+    'verify'         => 'تایید شده',
+    'yes'            => 'بله',
+
+    'flash'          => [
+        'success'   => 'موفق',
+        'error'   => 'خطا',
+        'warning' => 'اخطار',
+        'info'    => 'اطلاع',
+    ],
+
+];
---
+++ resources/lang/fa/messages.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language files for Messages
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default for all packages
+    |
+     */
+
+    'success'       => [
+        'created' => ':Module با موفقیت اضافه شد.',
+        'updated' => ':Module با موفقیت بروز رسانی شد.',
+        'deleted' => ':Module با موفقیت حذف شد.',
+        'changed' => ':Module به  :status تغییر یافت.',
+    ],
+
+    'error'         => [
+        'create' => ':Module قابل ایجاد نیست.',
+        'update' => ':Module قابل بروزرسانی نیست.',
+        'delete' => ':Module قابل حذف نیست.',
+    ],
+
+    'unprocessable' => 'ورودی غیر قابل پردازش',
+    'areyousure'    => 'آیا مطمعن هستید ؟',
+    'type'          => [
+        'success' => 'موفق',
+        'error'   => 'خطا',
+    ],
+];
