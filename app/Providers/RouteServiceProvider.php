6b729a42fa61c54d5e7da7795860f68c11d7c9b7
Arjun Shibu <arjunshibu1999@gmail.com>
2020-11-05 17:11:38 +0530

    Added security fix for lack of rate limiting

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -53,7 +55,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        Route::middleware('web')
+        // Applied the throttle middleware to rate-limit 20 request per IP for 15 minutes.
+        Route::middleware(['web', 'throttle:20,15'])
             ->namespace($this->namespace)
             ->group(base_path('routes/web.php'));
     }
@@ -68,7 +71,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::prefix('api')
-            ->middleware('api')
+            ->middleware(['api', 'throttle:20,15'])
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
     }
