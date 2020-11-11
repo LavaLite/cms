6ee79ece873ab68ddce38dbf211592f0db0ec74f
Arjun Shibu <arjunshibu1999@gmail.com>
2020-11-11 22:29:02 +0530

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -53,7 +53,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        // Applied the throttle middleware to rate-limit 20 request per IP for 15 minutes.
         Route::middleware(['web'])
             ->namespace($this->namespace)
             ->group(base_path('routes/web.php'));
