c72a7134f1fcce29b69f18151de7713dd0cac7ea
George John <george@renfos.com>
2021-08-22 11:39:24 +0400

    Update requests

---
+++ app/Http/Requests/PublicRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Litepie\Http\Request\AbstractRequest;
+
+class PublicRequest extends AbstractRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [];
+    }
+}
