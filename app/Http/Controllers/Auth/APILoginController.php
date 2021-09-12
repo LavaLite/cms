d21b4f24b94f5dd823b800b2418335df4d66efc1
George John <george@renfos.com>
2021-09-12 12:41:57 +0400

    Update  - Routes  - User languages  - Profile controllers  - .env examples

--- app/Http/Controllers/Auth/APILoginController.php
+++ app/Http/Controllers/Auth/APILoginController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller as BaseController;
-use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
--- routes/litepie.php
+++ routes/litepie.php
@@ -1,45 +1,48 @@
 <?php
 
-Route::prefix('{guard}/user')->group(function () {
-    Route::resource('user', 'Litepie\UserResourceController');
-    Route::resource('client', 'Litepie\ClientResourceController');
-});
-Route::get('{guard}/profile/{user}', 'Litepie\UserProfileController@profile');
-Route::get('{guard}/profile', 'Litepie\UserProfileController@profile');
-Route::post('{guard}/profile', 'Litepie\UserProfileController@postProfile');
-Route::get('{guard}/password', 'Litepie\UserProfileController@password');
-Route::post('{guard}/password', 'Litepie\UserProfileController@postPassword');
+Route::group(
 
-Route::prefix('{guard}/role')->group(function () {
-    Route::resource('role', 'Litepie\RoleResourceController');
-    Route::resource('permission', 'Litepie\PermissionResourceController');
-});
+    [
+        'prefix' => '{guard}',
+        'as' => 'guard.',
+        'where' => ['guard' => implode('|', array_keys(config('auth.guards')))],
+    ],
 
-Route::prefix('{guard}/team')->group(function () {
-    Route::resource('team', 'Litepie\TeamResourceController');
-});
+    function () {
+        Auth::routes();
+        Route::get('/', 'ResourceController@home')->name('home');
+        Route::get('profile', 'Auth\ProfileController@profile');
+        Route::post('profile', 'Auth\ProfileController@postProfile');
+        Route::get('password', 'Auth\ProfileController@password');
+        Route::post('password', 'Auth\ProfileController@postPassword');
+        Route::get('login/{provider}', 'Auth\SocialAuthController@redirectToProvider');
+        Route::get('profile/{user}', 'Auth\ProfileController@profile');
 
-// Guard routes for setting
-Route::prefix('{guard}/settings')->group(function () {
-    Route::get('/', 'Litepie\SettingResourceController@index');
-    Route::get('/{slug}', 'Litepie\SettingResourceController@show');
-    Route::post('/{type}', 'Litepie\SettingResourceController@store');
-});
+        Route::prefix('user')->group(function () {
+            Route::resource('user', 'Litepie\UserResourceController');
+            Route::resource('client', 'Litepie\ClientResourceController');
+        });
 
-Route::prefix('{guard}/masters')->group(function () {
-    Route::get('/', 'Litepie\MasterResourceController@index');
-    Route::get('{type}/list', 'Litepie\MasterResourceController@index');
-    Route::resource('master', 'Litepie\MasterResourceController');
-});
+        Route::prefix('role')->group(function () {
+            Route::resource('role', 'Litepie\RoleResourceController');
+            Route::resource('permission', 'Litepie\PermissionResourceController');
+        });
 
-// // Public routes for master
-// Route::get('masters/', 'Litepie\MasterPublicController@index');
-// Route::get('master/{slug?}', 'Litepie\MasterPublicController@show');
+        Route::prefix('team')->group(function () {
+            Route::resource('team', 'Litepie\TeamResourceController');
+        });
 
-// // Public routes for settings
-// Route::get('settings/', 'Litepie\SettingsPublicController@index');
-// Route::get('settings/{slug?}', 'Litepie\SettingsPublicController@show');
+        // Guard routes for setting
+        Route::prefix('settings')->group(function () {
+            Route::get('/', 'Litepie\SettingResourceController@index');
+            Route::get('/{slug}', 'Litepie\SettingResourceController@show');
+            Route::post('/{type}', 'Litepie\SettingResourceController@store');
+        });
 
-// // Public routes for users
-// Route::get('users/', 'Litepie\UsersPublicController@index');
-// Route::get('users/{slug?}', 'Litepie\UsersPublicController@show');
+        Route::prefix('masters')->group(function () {
+            Route::get('/', 'Litepie\MasterResourceController@index');
+            Route::get('{type}/list', 'Litepie\MasterResourceController@index');
+            Route::resource('master', 'Litepie\MasterResourceController');
+        });
+    }
+);
