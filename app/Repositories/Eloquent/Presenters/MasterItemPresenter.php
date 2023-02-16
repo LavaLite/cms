d45ca168b3316bd18ed91c65cb4e159d52b9374a
George John <george@renfos.com>
2023-02-16 23:33:56 +0530

    Remove repositories

--- app/Repositories/Eloquent/Presenters/MasterItemPresenter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories\Eloquent\Presenters;
-
-use Litepie\Master\Repositories\Eloquent\Presenters\MasterItemPresenter as BasePresenter;
-
-class MasterItemPresenter extends BasePresenter
-{
-}
--- app/Repositories/Eloquent/Presenters/RoleItemPresenter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories\Eloquent\Presenters;
-
-use Litepie\Roles\Repositories\Eloquent\Presenters\RoleItemPresenter as BasePresenter;
-
-class RoleItemPresenter extends BasePresenter
-{
-}
