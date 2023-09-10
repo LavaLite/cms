f869447f466cd1c14351139d14fcf4ee8acc1b89
nakamoto <59001254+nakamotojp@users.noreply.github.com>
2023-09-10 17:31:24 +0900

    Create app.php

---
+++ lang/ja/app.php
@@ -0,0 +1,79 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Label language files for All Module
+    |--------------------------------------------------------------------------
+    |
+     */
+
+    'name'           => 'Lavalite',
+    'name.html'      => '<b>Lava</b>lite',
+    'name.short'     => '<b>L</b>l',
+    'admin.panel'    => '管理画面',
+    'dashboard'      => 'ダッシュボード',
+    'rights'     => '<a href="https://lavalite.org/" target="_blank">Lavalite</a> &copy; 2018 All Rights Reserved,
+    Built with <a href="https://laravel.com" target="_blank"><i class="fab fa-laravel"></i></a>,
+    Created by <a href="http://renfos.com/" target="_blank">Renfos</a>.',
+    'version'        => '<b>Version</b> Develop',
+
+    'add'            => '追加',
+    'addnew'         => '新規作成',
+    'actions'        => 'アクション',
+    'approve'        => '承認',
+    'archive'        => 'アーカイブ',
+    'unarchive'      => 'アーカイブ解除',
+    'back'           => '戻る',
+    'cancel'         => 'キャンセル',
+    'close'          => '閉じる',
+    'copy'           => 'コピー',
+    'complete'       => '完了',
+    'create'         => '作成',
+    'dashboard'      => 'ダッシュボード',
+    'delete'         => '削除',
+    'draft'          => '草案',
+    'details'        => '詳細',
+    'edit'           => '編集',
+    'enterkeyword'   => '検索キーワードを入力してください。',
+    'go'             => '進む',
+    'help'           => 'ヘルプ',
+    'home'           => 'ホーム',
+    'list'           => 'リスト',
+    'logout'         => 'ログアウト',
+    'logs'           => '履歴',
+    'manage'         => '管理',
+    'more'           => '続きを読む',
+    'new'            => '新規',
+    'no'             => 'いいえ',
+    'opt'            => 'オプション（等）',
+    'option'         => 'オプション',
+    'options'        => 'オプション等',
+    'order'          => '注文',
+    'password'       => 'パスワード',
+    'profile'        => 'プロフィール',
+    'publish'        => '公開',
+    'request'        => '要求',
+    'reports'        => 'レポート',
+    'reset'          => 'リセット',
+    'save'           => '保存',
+    'search'         => '検索',
+    'settings'       => '設定',
+    'show'           => '表示',
+    'status'         => 'ステータス',
+    'update'         => '更新',
+    'update_profile' => 'プロフ更新',
+    'unpublish'      => '非公開',
+    'view'           => 'デザイン',
+    'verify'         => '確認済み',
+    'yes'            => 'はい',
+
+    'flash'          => [
+        'error'   => '成功',
+        'error'   => 'エラー',
+        'warning' => '注意',
+        'info'    => '情報',
+    ],
+
+];
